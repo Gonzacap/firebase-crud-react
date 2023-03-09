c68c8f42575a38da927af53cb9eae7212a62c384
Gonza <gonzalo.october@gmail.com>
2023-03-09 13:45:56 -0300

    add footer and header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import Links from './components/Links';
 
 function App() {
   return (
-    <div className="container p-4">
-      <div className='row'>
-        <Links />
-      </div>
-    </div>
+    <>
+      <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Links App</span>
+      </nav>
+      <main className="container p-4">
+        <div className='row'>
+          <Links />
+        </div>
+      </main>
+      <footer class="footer page-footer font-small blue pt-4 position-bottom" >
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://github.com/Gonzacap//"> Gonza Lopez</a>
+        </div>
+      </footer>
+    </>
   );
 }
 
